22938b9291e96354a5b67711bb77f31110851c53
SIVAJISAI <164608706+SIVAJISAI@users.noreply.github.com>
2025-09-19 14:15:37 +0530

    adding numbers upto given number

---
+++ sum_of_numbers.js
@@ -0,0 +1,9 @@
+const number = 5
+let dummyNumber = number
+let sum = 0;
+let nextNumber = 1
+while(nextNumber <= dummyNumber){
+    sum = sum + nextNumber
+    nextNumber = nextNumber + 1
+}
+console.log(sum);
