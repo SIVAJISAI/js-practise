644b7db8ccf460e650604caf14baadae85ec1d42
SIVAJISAI <164608706+SIVAJISAI@users.noreply.github.com>
2025-09-19 13:55:53 +0530

    control flow statement examples

---
+++ control_flow.js
@@ -0,0 +1,29 @@
+//big of two
+const x = 10;
+const y = 20
+let big = x;
+if(x<y){
+    big = y; 
+}
+ console.log(big,"is big number");
+const a= 10;
+const b= 20;
+const wow=a>b?10:20;
+console.log(wow)
+//login
+const name = "adminb";
+const pswd =12345;
+let state = "out"
+if(name === "admin" && pswd === 12345){
+    console.log("just logged",);   
+}
+ else{
+    console.log("not correct man");
+}
+//empty string checking
+const strings="hello bhayya";
+if (strings === ""){
+    console.log("empty")
+}else{
+    console.log("not empty")
+}
