5aa982978dfc0a9cd9a5a5ffa779f7c1a7981538
SIVAJISAI <164608706+SIVAJISAI@users.noreply.github.com>
2025-09-19 14:36:40 +0530

    reversing a string

--- revrse_string.js
+++ revrse_string.js
@@ -1,8 +1,8 @@
 const  string ="sir";
-let rev_string = "";
+let revString = "";
 let length = string.length;
 while (length >= 1){
-    rev_string = rev_string + string[length-1]
+    revString = revString + string[length-1]
     length--
 }
-console.log(rev_string,"is reverse of ",string)
+console.log(revString,"is reverse of ",string)
