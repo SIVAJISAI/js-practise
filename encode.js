ceb7cb2e25974ba5b3c07f953677f83577c1c17b
SIVAJISAI <164608706+SIVAJISAI@users.noreply.github.com>
2025-11-03 14:24:27 +0530

    divides into functions

--- encode.js
+++ encode.js
@@ -1,3 +1,7 @@
+function heading(text) {
+  console.log(`\n${"-".repeat(20) + text + "-".repeat(20)}`);
+}
+
 function encode(data) {
   if (typeof data === "number") {
     return "i" + data + 'e';
@@ -32,26 +36,36 @@ function TestBencode(description, data, expectedOutput) {
   console.log(messageToPrint(description, data, result, expectedOutput));
 }
 
-function testDecode() {
-  decode("a normal number", "i123e", 123)
-}
-
-function testAll() {
+function testNumbers() {
+  heading("NUMBERS");
   TestBencode("number 123", 123, 'i123e');
   TestBencode("number 456", 456, 'i456e');
   TestBencode("number 0", 0, 'i0e');
   TestBencode("negative numbers", -86, 'i-86e');
+}
+
+function testString() {
+  heading("STRING");
   TestBencode("string hello", "hello", '5:hello');
   TestBencode("empty string", "", '0:');
   TestBencode("string bye bye", "bye bye", '7:bye bye');
-  TestBencode("string bye bye", "special!@#$chars", "16:special!@#$chars");
+  TestBencode("long string", "special!@#$chars", "16:special!@#$chars");
+}
+
+function testList() {
+  heading("LIST");
   TestBencode("normal list", [0], "li0ee");
   TestBencode("empty list", [], "le");
   TestBencode("list with number and string", [0, 'hello'], "li0e5:helloe");
   TestBencode("list inside a list", [0, 'hello', ["abc"]], "li0e5:hellol3:abcee");
   TestBencode("nested list inside a list", ["one", ["two", ["three"]]], "l3:onel3:twol5:threeeee");
   TestBencode("empty list inside a list", ["", 0, []], "l0:i0elee");
-  testDecode();
+}
+
+function testAll() {
+  testNumbers();
+  testString();
+  testList();
 }
 
 testAll();
